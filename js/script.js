6e506964d707f3b93fb1e352976e3e92edc179eb
pumpushka <turletskaoksana@gmail.com>
2016-07-26 13:55:20 +0300

    navigation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 
 var menu = document.querySelector('#menu');
-var navigation = document.querySelector('.nav');
+var navigation = document.querySelector('.side-menu');
+var menu_close = document.querySelector('.menu-close_link');
 
 menu.addEventListener('click', function(e) {
-  navigation.classList.toggle('open');
+  navigation.classList.toggle('visible');
   e.stopPropagation();
+});
+
+menu_close.addEventListener('click', function(e) {
+  navigation.classList.toggle('hidden');
 });
